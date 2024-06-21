a16293499b1141bfb15072392b16a3bff48a5798
Gustavo <gc722793@gmail.com>
2024-06-21 14:34:48 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection()
+}
+
+function activateMenuAtCurrentSection () {
+  const targetline = scrollY + innerHeight / 2
+}
+
+function showNavOnScroll() {
+  const navigation = document.querySelector("#navigation")
+
+  if (scrollY > 1) {
+    navigation.classList.add("scroll")
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+} 
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats,
+  #services,
+  #services header,
+  #services .cards .card,
+  #about, 
+  #about header, 
+  #about .content
+`)
